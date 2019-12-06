4840c7d97f20992860494ab6733bfb2c17a2df50
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-12-06 01:46:27 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith('join')) {
     message.delete(1000);
- message.channel.send(`Oko :)`);
+ message.channel.send(`Que jotos :)`);
 	}
 });
 
