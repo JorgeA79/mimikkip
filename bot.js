95fb0847dc013e4edaf03ca552ddcafea4b6de07
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-12-06 01:36:07 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,17 +8,8 @@ var prefix = "s."
 client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith('join')) {
- 
- const channel = client.channels.get("594033961097887775");
-		
-  if (!channel) return console.error("The channel does not exist!");
-  channel.join().then(connection => {
-    // Yay, it worked!
-    console.log("Successfully connected.");
-  }).catch(e => {
-    // Oh no, it errored! Let's log it to console :)
-    console.error(e);
-  });
+    message.delete(1000);
+ message.channel.send(`NO`);
 	}
 });
 
