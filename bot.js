5854f3f500577356e81eae7147791f93760cda89
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-12-06 01:44:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith('join')) {
     message.delete(1000);
- message.channel.send(`Quien es el joto`);
+ message.channel.send(`Oko :)`);
 	}
 });
 
