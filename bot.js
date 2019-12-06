8f06fe2eebfb9bff7c4d6650b75322e9b018fe77
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-12-06 01:19:57 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready',() => {
-	//Edit after game so u can add wut game bot is playing between "" xd
-	   client.user.setPresence({game: {name: "", type: 0}});
-});
 
 var prefix = "s."
 
 client.on('message', message => {
-	if (message.author === client.user) return;
+	if (message.author == client.user) return;
 	if (message.content.startsWith((prefix) + 'join')) {
 
 
