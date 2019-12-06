8280476dc5a2d312a0dc01755eaf0f275294f8c6
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2019-12-06 01:29:14 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,15 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith('join')) {
  
-client.channels.get('594033961097887775').join();
+ const channel = client.channels.get("594033961097887775");
+  if (!channel) return console.error("The channel does not exist!");
+  channel.join().then(connection => {
+    // Yay, it worked!
+    console.log("Successfully connected.");
+  }).catch(e => {
+    // Oh no, it errored! Let's log it to console :)
+    console.error(e);
+  });
 	}
 });
 
