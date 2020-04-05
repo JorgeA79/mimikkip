389445e3dcbae1159c4ffaa930fe32519371dda2
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-04-04 20:17:44 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'owo')) {
 		var v = message.member.username;
-		 message.channel.send(message.member.user.tag + `xd`);
+		 message.channel.send(message.member.user + `xd`);
 	}
 });
 client.on('message', message => {
