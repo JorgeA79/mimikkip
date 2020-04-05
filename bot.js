9008cf709a54e513389fbb0cf5bf96b98c9744bf
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-04-04 21:32:38 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,8 +37,8 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', member => {
- member.guild.channels.get('696193926633095199').send('xd');
-	 member.setNickname(`OWO // ${member.user}`);
+ member.guild.channels.get('696193926633095199').send('xd'+member.username);
+	 member.setNickname(`OWO // `);
 });
 	  
 //Important
