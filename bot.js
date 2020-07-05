2bd6afa87493e2f92f2e393a43a1ac2e2017ecff
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-05 15:00:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,39 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var prefix = "p!";
+var waifus = new Map();
 
-client.on('ready',() => {
-	
-	   client.user.setPresence({game: {name: "with Tanjiro", type: 0}});
- 	   client.user.setUsername("Nezuko");
-});
-
-var prefix = "n!"
-
-client.on('message', message => {
+client.on('message', async msg => {
 	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'ping')) {
+	if (message.author.bot) return;
+	const waifuServer = waifus.get(msg.guild.id);
 	
-		 message.channel.send(`:ping_pong: Pong! \nTime taken: \`${Date.now() - message.createdTimestamp} ms\``);
+	if(!waifuServer){
+	const waifuConstruct = {
+		textChannel: msg.channel,
+		number:	generateWaifu().		
+	};
+	waifus.set(msg.guild.id, waifuConstruct);
+	
+	try{
+	waifuServer.waifuConstruct.textChannel.send('xd')
+	}catch(error){
+	console.error(error)
+	}			
+	}else{
+		
+	waifuServer.waifuConstruct.textChannel.send('xdxd')
+	}
+	
 	}
 });
 
-client.on('message', message => {
-	
- 	if (message.channel.id === "696466093018972161"){
-	if (message.author.bot){
-	if(message.author.id === "125367104336691200") {
-		
-	
-		
-		message.channel.send(`<@&696466852196646942>, New episode!`);
-	}}}
-});
+function generateWaifu(){
+let min = 10;
+let max = 30;  
+return Math.floor(Math.random()*(max - min+1)) + 10;
+}
 
-client.on('guildMemberAdd', member => {
-	 var j = member.displayName;
-	 member.setNickname(`🖤 - `+ j);
-});
-	  
-//Important
+
 client.login(process.env.BOT_TOKEN);
