f70993a678dcb0961f10896e67c7d1d9df6668b6
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-05 18:51:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,9 +34,9 @@ client.on('message', async msg => {
 	
 	if(waifuServer.currentPoints == waifuServer.number){	
 		
-	if(waifuServer.mSent == 0){		
-	waifuServer.mSent == 1;	
-		
+	if(waifuServer.mSent == 1) return;		
+	
+	waifuServer.mSent == 1;		
 	var waifu = jsonContent.waifus;
 	const filter = m => m.content.includes(`p!get ${waifu[waifuServer.waifu].name}`);
 		
@@ -54,11 +54,8 @@ client.on('message', async msg => {
 			msg.channel.send('Looks like nobody got the answer this time.');
 			waifus.delete(msg.guild.id);
 		});
-	});
+	});		
 		
-	}else {
-	return;
-	}		
 	}else{
 	const points = eval(waifuServer.currentPoints) + eval(1);
 	waifuServer.currentPoints = points;	
