e93a4bd985844eb93edd3cc36ce9283dbd342700
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-05 18:01:29 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 var prefix = "p!";
 var waifus = new Map();
 
-var contents = fs.readFileSync("./commands/agents/agents.json");
+var contents = fs.readFileSync(".waifus.json");
 var jsonContent = JSON.parse(contents);
 
 client.on('message', async msg => {
