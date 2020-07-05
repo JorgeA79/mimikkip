92bcdb8df323a5af7cb6bd83930f9981132938b9
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-05 16:44:54 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,25 +12,34 @@ client.on('message', async msg => {
 	if(!waifuServer){
 	const waifuConstruct = {
 		textChannel: msg.channel,
-		number:	generateWaifu()		
+		number:	generateWaifu(),
+		initialPoints: 0,
+		currentPoints: 1
 	};
 	waifus.set(msg.guild.id, waifuConstruct);
 	
 	try{
 	
-	msg.channel.send(waifuConstruct.number);		
+	msg.channel.send(waifuConstruct.number & waifuConstruct.currentPoints);		
 		
 	}catch(error){
 	console.error(error)
 	}			
 	}else{
-	msg.channel.send(waifuServer.number);		
+	if(waifuServer.currentPoints == waifuServer.number){		
+	msg.channel.send('Waifu!');	
+	waifus.delete(msg.guild.id);	
+	}else{
+	const points = eval(waifuServer.currentPoints) + eval(1);
+	waifuServer.currentPoints = points;	
+	msg.channel.send(waifuServer.number & waifuServer.currentPoints);			
+	}
 	}
 });
 
 function generateWaifu(){
-let min = 10;
-let max = 30;  
+let min = 1;
+let max = 3;  
 return Math.floor(Math.random()*(max - min+1)) + 10;
 }
 
